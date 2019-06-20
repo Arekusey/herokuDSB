6e9996eb379b6b55659ba206b8b0b5a4d0a173da
Arekusey <ykpon5@yandex.ru>
2019-06-20 18:45:13 +0300

    Add files

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+Client.on('ready', () => {
+  console.log("Bot is running");
+});
+
+Client.on('message', (message) => {
+  if(message.author.bot) return;
+  message.reply(`Hello`);
+});
+
+Client.login(process.env.BOT_TOKEN);
